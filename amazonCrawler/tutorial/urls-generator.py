f72bec3c58805bbad20f4905af5bbd08219170ff
Hamid <hamid42643@gmail.com>
2014-09-10 18:52:44 -0300

    first commit

---
+++ amazonCrawler/tutorial/urls-generator.py
@@ -0,0 +1,4 @@
+    
+for x in range(1, 9):
+    #print '"http://www.amazon.com/Canon-PowerShot-A2500-Stabilized-2-7-Inch/product-reviews/B00B5HE2UG/ref=cm_cr_pr_top_link_2?ie=UTF8&filterBy=addFiveStar&pageNumber=%d&showViewpoints=0&sortBy=bySubmissionDateDescending", ' % (x)
+    print '"http://www.amazon.com/Kodak-C1530-Digital-Camera-White/product-reviews/B004MB8302/ref=cm_cr_pr_top_link_2?ie=UTF8&filterBy=addFourStar&pageNumber=%d&showViewpoints=0&sortBy=bySubmissionDateDescending", ' % (x)
